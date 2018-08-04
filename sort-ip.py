513241fef1d5a439c5ab421fbfbbdb197a776677
Damian Torres <datorr2@gmail.com>
2018-08-04 00:54:30 -0400

    [sort-ip.py] v1.1

--- sort-ip.py
+++ sort-ip.py
@@ -7,7 +7,7 @@ from __future__ import print_function
 
 __author__  = "Damian Torres"
 __email__   = "datorr2@gmail.com"
-__version__ = "1.0"
+__version__ = "1.1"
 
 import sys
 import os
@@ -35,14 +35,14 @@ IPV4_VALIDATION_REGEX       = re.compile("(?<!\d)(?:(?:(?:22[0-3]|2[01]\d|1[" \
     + "]|2[0-4]\d|1\d{2}|[1-9]?\d))|(?:172\.(?:25[0-5]|2[0-4]\d|1\d{2}|[4-9]" \
     + "\d|3[2-9]|1[0-5]|\d))|(?:192\.(?:25[0-5]|2[0-4]\d|16[0-79]|1[0-57-9][" \
     + "0-9]|[1-9]?\d))|(?:169\.(?:25[0-35]|2[0-4]\d|1\d{2}|[1-9]?\d)))(?:\.(" \
-    + "?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)){2}(?!\d)")
+    + "?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)){2}(?!\d)(?:/(?:3[0-2]|[0-2]?\d))?")
 IPV6_VALIDATION_REGEX       = re.compile("(?<![0-9a-f:])(?:[0-9a-f]{1,4}(?::" \
     + "[0-9a-f]{1,4}){7}|::[0-9a-f]{1,4}(?::[0-9a-f]{1,4}){0,6}|(?:[0-9a-f]{" \
     + "1,4}:){1,7}:|[0-9a-f]{1,4}:(?::[0-9a-f]{1,4}){0,6}|(?:[0-9a-f]{1,4}:)" \
     + "{2}(?::[0-9a-f]{1,4}){1,5}|(?:[0-9a-f]{1,4}:){3}(?::[0-9a-f]{1,4}){1," \
     + "4}|(?:[0-9a-f]{1,4}:){4}(?::[0-9a-f]{1,4}){1,3}|(?:[0-9a-f]{1,4}:){5}" \
     + "(?::[0-9a-f]{1,4}){1,2}|(?:[0-9a-f]{1,4}:){6}:[0-9a-f]{1,4})(?![0-9a-" \
-    + "f:])")
+    + "f:])(?:/1(?:2[0-8]|[01]\d)|\d{1,2})?")
 
 
 # # # # # # #
@@ -54,23 +54,42 @@ def eprint(*args, **kwargs):
     print (*args, file=sys.stderr, **kwargs)
 
 
-# Print usage
+# Return version information
+def version():
+    return """csv.py - v{version}
+Written by {author} ({email})""".format(**{
+        "version"   : __version__,
+        "author"    : __author__,
+        "email"     : __email__
+    })
+
+
+# Return usage information
 def usage():
-    print(USAGE_HELP)
-    return
+    return USAGE_HELP
 
 
 # Handle command-line options and arguments
 def optionsHandler():
-    parser = argparse.ArgumentParser(add_help=True,
-      description="Parse and lookup IPv4 address netblocks and their " \
+    parser = argparse.ArgumentParser(add_help=False,
+      description="Parse and lookup IP address netblocks and their " \
       + "respective owners")
-    parser.add_argument("-V", "--version", action="version",
+    parser.add_argument("-h", "--help", action="store_true",
+      help="display this help and exit")
+    parser.add_argument("-V", "--version", action="store_true",
       help="output version information and exit")
     parser.add_argument('filename', metavar='file', type=str, nargs="?",
       help="input filename (default: stdin)")
 
     args = parser.parse_args()
+
+    if args.version:
+        print(version())
+        exit(0)
+
+    if args.help:
+        print(usage())
+        exit(0)
 
     return args.filename
 
@@ -87,7 +106,15 @@ def parseIPv4Addresses(fn):
     for line in fh:
         matches = IPV4_VALIDATION_REGEX.findall(line)
         for match in matches:
-            ipObj = ipaddress.IPv4Address(unicode(match))
+            if sys.version_info < (3, 0):
+                ip = unicode(match)
+            else:
+                ip = match
+
+            if "/" in ip:
+                ipObj = ipaddress.IPv4Network(ip)
+            else:
+                ipObj = ipaddress.IPv4Address(ip)
             dictObj[ipObj] = 1
 
     myArr = list(dictObj.keys())
@@ -107,7 +134,15 @@ def parseIPv6Addresses(fn):
     for line in fh:
         matches = IPV6_VALIDATION_REGEX.findall(line)
         for match in matches:
-            ipObj = ipaddress.IPv6Address(unicode(match))
+            if sys.version_info < (3, 0):
+                ip = unicode(match)
+            else:
+                ip = match
+
+            if "/" in match:
+                ipObj = ipaddress.IPv6Network(ip)
+            else:
+                ipObj = ipaddress.IPv6Address(ip)
             dictObj[ipObj] = 1
 
     myArr = list(dictObj.keys())
@@ -124,10 +159,10 @@ def main():
     ipv4_addys = parseIPv4Addresses(fn)
     ipv6_addys = parseIPv6Addresses(fn)
 
-    for addy in sorted(ipv4_addys):
+    for addy in sorted(ipv4_addys, key=ipaddress.get_mixed_type_key):
         print(addy)
 
-    for addy in sorted(ipv6_addys):
+    for addy in sorted(ipv6_addys, key=ipaddress.get_mixed_type_key):
         print(addy)
 
     return
